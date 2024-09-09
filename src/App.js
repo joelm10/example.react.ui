df7e2cb4623dd60ad09ddce301580c561a422c24
Joel Morrison <joel.morrison@myob.com>
2024-09-09 16:36:32 +1000

    fix: fix prev commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// layout
+import Layout from './components/layout';
+
+// TODO: implement react-dom routing
+const App = () => {
+    const appWrapper = (
+        <Layout />
+    );
+    return appWrapper;
+};
+
+export default App;
