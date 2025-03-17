1236458076ef45d32e2ec266d73d9ddc4cf85df4
Joel Morrison <joel.morrison@myob.com>
2025-03-17 15:41:34 +1100

    wip: simple test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,12 +2,14 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom'
 
 import App from 'App';
+describe('Base Application', () => {
+    test('<App /> should render correctly', () => {
+        render(
+            <BrowserRouter>
+                <App tab="home" pageTitle='test page title here' />
+            </BrowserRouter>
+        );
+        // TODO: Add assert
+    });
 
-it('renders without crashing', () => {
-    render(
-        <BrowserRouter>
-            <App tab="home" pageTitle='test page title here' />
-        </BrowserRouter>
-    );
-    // TODO: Add assert
 });
--- src/__tests__/AppBody.test.js
+++ src/__tests__/AppBody.test.js
@@ -3,13 +3,39 @@ import { BrowserRouter } from 'react-router-dom'
 
 import AppBody from "layout/appBody";
 
+delete global.window.location;
+global.window = Object.create(window);
+
+
+const cases = ['/home', '/about', '/engineering', '/game', '/chart'];
+
+
 describe('Base Application', () => {
+    beforeEach(() => {
+        delete global.window.location;
+        global.window = Object.create(window);
+    });
+
     test('<AppBody />, should render correctly', () => {
+        global.window.location = {
+            pathname:
+                ''
+        };
+
         render(
             <BrowserRouter>
                 <AppBody />
             </BrowserRouter>
         );
-
     });
+
+    test.each(cases)(`<AppBody />, should render correctly when activePage IS set to %p]`,
+        (testCase) => {
+            global.window.location = { pathname: testCase };
+            render(
+                <BrowserRouter>
+                    <AppBody />
+                </BrowserRouter>
+            );
+        });
 })
